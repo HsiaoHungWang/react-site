453bc1d9009ce7d9d0a48ae5e8480bc1ec27a2e0
HsiaoHung Wang <wang@ispan.com.tw>
2022-05-12 15:37:09 +0800

    Home、About、Contact Page

---
+++ src/Pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About(){
+    return(
+        <>
+        <h3>About Page</h3>
+        </>
+    )
+}
+
+export default About;
---
+++ src/Pages/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Contact(){
+    return(
+        <>
+        <h3>Contact Page</h3>
+        </>
+    )
+}
+
+export default Contact;
