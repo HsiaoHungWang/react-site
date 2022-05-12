eb7829f680e40e4a87695c2c06d41a4ff7ec6f38
HsiaoHung Wang <wang@ispan.com.tw>
2022-05-12 15:49:42 +0800

    React 的路由用法

--- src/App.js
+++ src/App.js
@@ -7,15 +7,28 @@ import Contact from './Pages/Contact';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import { Container,Row,Col } from 'react-bootstrap';
+import { Link, Route, Routes } from 'react-router-dom';
 
 //JSX
 function App() {
   return (
     <Container>
-      <Home />
-      <About />
-      <Contact />
+     <header>
+       <nav>
+         <Link to="/">Home</Link> | 
+         <Link to="/about">About</Link> |      
+         <Link to="/contact">Contact</Link> | 
+       </nav>
+     </header>
 
+      <Routes>
+        {/* http://localhost:3000/ */}
+        <Route path="/" element={ <Home />} />
+          {/* http://localhost:3000/about */}
+        <Route path="/about" element={<About />} />
+          {/* http://localhost:3000/contact */}
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
   </Container>
   );
 }
